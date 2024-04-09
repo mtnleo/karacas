55285c0464e25836cac741d98767a27894244e48
mtnleo <mtnleonardi@gmail.com>
2024-04-09 14:18:20 -0300

    New foundations created for a web to encrypt and decrypts Karaca's

---
+++ funcs.js
@@ -0,0 +1,7 @@
+document.getElementById("encrypt").onclick = () => {
+    document.getElementById("title").innerText = "Karaca's Encryption Algorithm";
+}
+
+document.getElementById("decrypt").onclick = () => {
+    document.getElementById("title").innerText = "Karaca's Decryption Algorithm";
+}
