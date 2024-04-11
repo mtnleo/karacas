75e08707100ce22697fdd136d91cafc823d165d3
mtnleo <mtnleonardi@gmail.com>
2024-04-11 20:58:22 -0300

    Karaca's encryptinmg algorithm working, some polishing to do to the other stuff

--- funcs.js
+++ funcs.js
@@ -1,5 +1,5 @@
-let kType = 0; // 0 -> Decryption | 1 -> Encryption
-
+let kType = 1; // 0 -> Decryption | 1 -> Encryption
+const letterValues = ["a", "e", "i", "o", "u"];
 
 document.getElementById("encrypt").onclick = () => {
     document.getElementById("title").innerText = "Karaca's Encryption Algorithm";
@@ -13,28 +13,75 @@ document.getElementById("decrypt").onclick = () => {
 
 document.getElementById("search").onclick = () => {
     if (kType === 0) {
-        document.getElementById("output").innerText = decrypt("0p0paca");
+        
+        document.getElementById("output").innerText = decrypt(document.forms["inputForm"]["inputText"].value);
     }
     else {
-        document.getElementById("output").innerText = encrypt();
+        document.getElementById("output").innerText = encrypt(document.forms["inputForm"]["inputText"].value);
     }
     
     
 } 
 
 
-
 function reverseWord(word) {
-    console.log(word);
+    let newWord = "";
+
+    for (let i = word.length - 1; i >= 0; i--) {
+        newWord += word[i];
+    }
+
+    return newWord;
 }
+
+function encLetters(word) {
+    let newWord = "";
+    for (let i = 0; i < word.length; i++) {
+        if (!isNumber(word[i])) {
+            if (letterValues.indexOf(word[i].toLowerCase()) >= 0) {
+                newWord += letterValues.indexOf(word[i].toLowerCase());
+            }
+            else {
+                newWord += word[i];
+            }
+        }
+    }
+    return newWord;
+}
+
+function isNumber(character) {
+    return /\d/.test(character);
+}
+
+function decLetters(word) {
+    let newWord = "";
+    for (let i = 0; i < word.length; i++) {
+        if (isNumber(word[i])) {
+            newWord += letterValues[word[i]];
+        }
+        else {
+            newWord += word[i];
+        }
+    }
+    return newWord;
+}
+
+
 
 function decrypt(word) {
     word = word.slice(0, -3);
-    let revWord = reverseWord(word);
+    let modWord = reverseWord(word);
+    modWord = decLetters(modWord);
+
+    return modWord;
 
 }
 
 function encrypt(word) {
-    let revWord = ;
-    revWord = word + "aca";
+    let modWord = reverseWord(word); // Reverse word
+    console.log("Word reversed: " + modWord);
+    modWord = encLetters(modWord);   // Encrypt letters
+    console.log("Encrypted word: " + modWord)
+    modWord = modWord + "aca";
+    return modWord;
 }
